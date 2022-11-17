0909e5bd9a54c0a2362448d69d8f55a57d305d62
unknown <Administrator@TAF-LT548.taf.local>
2022-11-17 21:25:43 +0300

    my first node js app

---
+++ server.js
@@ -0,0 +1,50 @@
+// const express = require('express');
+// const app = express()
+
+// app.get('/', function(req,res) {
+//     res.send("Hello World");
+// });
+
+// app.listen(8080, function() {
+//     console.log("Server running on port 3001");
+// })
+
+var http=require('http');
+var fs=require('fs');
+
+http.createServer((req,res)=>{
+    if(req.url==='/'){
+        fs.readFile('index.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url==='/about'){
+        fs.readFile('about.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url==='/contact-me'){
+        fs.readFile('contact-me.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else{
+        fs.readFile('404.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+}).listen(8080,()=>{
+    console.log('Listeninig on port  8080.....');
+});
